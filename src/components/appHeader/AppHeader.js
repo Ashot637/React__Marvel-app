9307031b300c179ecdf3abde37759f72a909b266
Ashot <ashotmartirosyan637@gmail.com>
2022-12-25 19:18:47 +0400

    commit

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -5,14 +5,14 @@ const AppHeader = () => {
     return (
         <header className="app__header">
             <h1 className="app__title">
-                <Link to="/React__Marvel-app" >
+                <Link to="/" >
                     <span>Marvel</span> information portal
                 </Link>
             </h1>
             <nav className="app__menu">
                 <ul>
                     <li><NavLink
-                    to="/React__Marvel-app" 
+                    to="/" 
                     style={({ isActive }) => (isActive ? {color: '#9F0013'} : null)}>Characters</NavLink>
                     </li>/
                     <li><NavLink 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './components/app/App';
-import { HashRouter } from 'react-router-dom';
-
 import './style/style.scss';
 
-ReactDOM
-    .createRoot(document.getElementById('root'))
-    .render(
-      <HashRouter>
-        <App />
-      </HashRouter>
-    );
+ReactDOM.render(
+      <App />,
+    document.getElementById('root')
+)
+
+
