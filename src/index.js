aab2675c511bb9bf41db93e6c1545f9f01cb75a7
Ashot <ashotmartirosyan637@gmail.com>
2022-12-25 19:08:13 +0400

    hash router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
+import { HashRouter } from 'react-router-dom';
 
 import './style/style.scss';
 
 ReactDOM
     .createRoot(document.getElementById('root'))
     .render(
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     );
