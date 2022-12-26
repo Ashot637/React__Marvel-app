e01727dc7c1ee7783b3d36c60e943e56ac347033
Ashot <ashotmartirosyan637@gmail.com>
2022-12-26 23:54:58 +0400

    test

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -5,18 +5,18 @@ const AppHeader = () => {
     return (
         <header className="app__header">
             <h1 className="app__title">
-                <Link to="/React__Marvel-app">
+                <Link to="/">
                     <span>Marvel</span> information portal
                 </Link>
             </h1>
             <nav className="app__menu">
                 <ul>
                     <li><NavLink
-                    to="/React__Marvel-app" 
+                    to="/" 
                     style={({ isActive }) => (isActive ? {color: '#9F0013'} : null)}>Characters</NavLink>
                     </li>/
                     <li><NavLink 
-                    to="/React__Marvel-app/comics" 
+                    to="/comics" 
                     style={({ isActive }) => (isActive ? {color: '#9F0013'} : null)}>Comics</NavLink></li>
                 </ul>
             </nav>
